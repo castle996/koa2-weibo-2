a409927e3c41b12ae08751990d7662e30a165b12
castle996 <bobxia84@gmail.com>
2021-08-18 15:48:18 +0800

    feat:relation单元测试

--- test/blog/home.test.js
+++ test/blog/home.test.js
@@ -4,7 +4,7 @@
  */
 
  const server=require('../server')
- const {COOKIE}=require('../testUserInfo')
+ const {Z_COOKIE}=require('../testUserInfo')
  let BLOG_ID=''
  //new blog
  test('new blog,应该成功',async()=>{
@@ -14,7 +14,7 @@
      const res=await server
      .post('/api/blog/create')
      .send({content,image})
-     .set('cookie',COOKIE)
+     .set('cookie',Z_COOKIE)
      expect(res.body.errno).toBe(0)
      expect(res.body.data.content).toBe(content)
      expect(res.body.data.image).toBe(image)
--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -8,7 +8,11 @@
  * 每次测试用户重新登录，需要更新cookie
  */
  module.exports={
-    COOKIE:'weibo.sid=vnRd_PpNLn29hyk2dNdVnlJGLbwKpLTE; weibo.sid.sig=fyH5mcly_RrnMxfetjwRK0-f6Hc',
-    USER_NAME:'zhangsan'
+    Z_COOKIE:'weibo.sid=Zt9pDoSk-QVY-XgNTqhERGoD-7oL007H; weibo.sid.sig=U1IBK5PqLpMv3PTTVVlYYYr6dvQ',
+    Z_USER_NAME:'zhangsan',
+    Z_ID:1,
+    L_COOKIE:'weibo.sid=Zt9pDoSk-QVY-XgNTqhERGoD-7oL007H; weibo.sid.sig=U1IBK5PqLpMv3PTTVVlYYYr6dvQ',
+    L_USER_NAME:'lisi',
+    L_ID:2
  }
  
